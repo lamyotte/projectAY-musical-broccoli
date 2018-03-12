c5973580ccd18b1deed215b745e3bf08ae3c6175
Laurence Amyotte <laurenceamyotte@gmail.com>
2018-03-12 11:52:24 -0400

    endTurn tests

--- server_services/main_service/test/gameTests_endTurn.js
+++ server_services/main_service/test/gameTests_endTurn.js
@@ -1,14 +1,63 @@
-var assert = require('assert');
+var assert = require('chai').assert;
+var attack = require('../game_manager.js').attack;
+var endTurn = require('./data/gameDataDefault').endTurn;
+
 describe('EndTurn tests', () => {
   describe('EndTurn', () => {
-    it('should end turn on empty boards', () => {
-      assert.equal([1,2,3].indexOf(4), -1);
+  	let gameData;
+	beforeEach(() => {
+		gameData = JSON.parse(JSON.stringify(defaultGameData));
+	});
+    it('should end turn on empty boards no mana', () => {
+    	let message = { 
+    		playerId: 1
+    	}
+    	endTurn(message, gameData, (message) => {console.log(message)});
+    	assert.isTrue(gameData.playing == 'player2');
+    	assert.isTrue(gameData.player2.powerActions == 1);
+    	assert.isTrue(gameData.player2.mana == 1);
+    	assert.isTrue(gameData.player2.manapool == 1);
     });
-    it('should end turn with creatures on board', () => {
-      assert.equal([1,2,3].indexOf(4), -1);
-    });
-    it('should end turn with end turn special', () => {
-      assert.equal([1,2,3].indexOf(4), -1);
-    });
+	it('should end turn on empty boards max mana', () => {
+		gameData.player2.manapool = 10;
+		let message = { 
+			playerId: 1
+		}
+		endTurn(message, gameData, (message) => {console.log(message)});
+		assert.isTrue(gameData.playing == 'player2');
+    	assert.isTrue(gameData.player2.powerActions == 1);
+		assert.isTrue(gameData.player2.mana == 10);
+		assert.isTrue(gameData.player2.manapool == 10);
+	});
+	it('should end turn with creatures on board', () => {
+		gameData.player2.board.push({
+			uid: '12',
+			actions: 0
+		})
+		let message = { 
+			playerId: 1
+		}
+		endTurn(message, gameData, (message) => {console.log(message)});
+		assert.isTrue(gameData.playing == 'player2');
+    	assert.isTrue(gameData.player2.board[0].actions == 1);
+    	assert.isTrue(gameData.player2.mana == 1);
+    	assert.isTrue(gameData.player2.powerActions == 1);
+    	assert.isTrue(gameData.player2.manapool == 1);
+	});
+	it('should end turn with end turn special', () => {
+		gameData.player2.board.push({
+			uid: '12',
+			actions: 0
+		})
+		let message = { 
+			playerId: 1
+		}
+		endTurn(message, gameData, (message) => {console.log(message)});
+		assert.isTrue(gameData.playing == 'player2');
+    	assert.isTrue(gameData.player2.board[0].actions == 1);
+    	assert.isTrue(gameData.player2.mana == 1);
+    	assert.isTrue(gameData.player2.powerActions == 1);
+    	assert.isTrue(gameData.player2.manapool == 1);
+	});
   });
 });
